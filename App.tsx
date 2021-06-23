6d552272cab071789b1320aa52256e846654ba59
sousa-gabriel <sousa-gabriel@hotmail.com>
2021-06-23 15:32:22 -0300

    Instalando Fontes no app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+//comando para importar libs do google (expo install expo-font @expo-google-fonts/inter)
+import {useFonts} from 'expo-font';
+import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter';
+import {Rajdhani_500Medium,Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+//comando para importar o loading (expo install expo-app-loading)
+import AppLoading from 'expo-app-loading';
 import {SignIn} from './src/screens/SignIn';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+  if(!fontLoaded){
+    return <AppLoading/>
+  }
   return (
     <SignIn />
   );
